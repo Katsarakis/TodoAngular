cbd81223646965207b78ae1946aa5ed48b8bb059
mkatsarakis <m.katsarakis@kunbus.com>
2023-06-19 15:43:42 +0200

    commit v. 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Itodo } from './itodo';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    const data = localStorage.getItem("todos");
+    if (data !== "" && data !== null) {
+      this.todos = JSON.parse(data);
+    }
+  }
+
+  constructor(private loggingservice: LoggingService) {
+
+  }
+
+  todos: Itodo[] = [];
+  newTodo = '';
+
+  logging(msg: any) {
+    this.loggingservice.log(msg);
+  }
+
+  setTodo(event: KeyboardEvent)
+  {
+    this.newTodo = (event.target as HTMLInputElement).value;
+    // console.log(this.newTodo);
+  }
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push({ todo: this.newTodo, done: false});
+    }
+    
+    this.storeTodos();
+
+    this.logging(this.newTodo);
+  }
+
+  countOpenTodos() {
+    const doneArray = this.todos.filter((item) => {
+      return !item.done;
+    })
+    return doneArray;
+  }
+
+  storeTodos() {
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
+
+  toggleTodo(index: number) {
+    this.todos[index].done = !this.todos[index].done; 
+
+    this.storeTodos();
+  }
+
+  deleteTodo(index: number) {
+    this.todos.splice(index, 1); 
+
+    this.storeTodos();
+  }
+
   title = 'todoapp';
 }
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+
+export class TodoComponent implements OnInit {
+  @Input() todo = { todo: '', done: false };
+  @Input() index = 0;
+
+  @Output() todoIndex = new EventEmitter<number>();
+  @Output() deleteIndex = new EventEmitter<number>();
+
+  toggleTodo(){
+    // console.log(this.index);
+    // debugger;
+    this.todoIndex.emit(this.index);
+  }
+
+  deleteTodo(){
+    this.deleteIndex.emit(this.index);
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
+
+}
